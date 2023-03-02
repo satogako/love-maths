9400dd2668501a02284ae872fa81472c582f8d73
satogako <genevaq@ukr.net>
2023-03-02 22:17:49 +0000

    Add code to increment score and incorrect answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert('Hey! You got it right! :D')
+        incrementScore();
     } else {
         alert(`Awwww....you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -70,11 +72,23 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current tally of incorect answers from the DoM and increments it by 1
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 
 }
 
