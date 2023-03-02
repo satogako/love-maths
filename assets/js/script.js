ba194fda0bb99c1ffb8acce1d9e52b37f8c1c453
satogako <genevaq@ukr.net>
2023-03-02 17:27:01 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,18 +10,27 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert('You  submit!')
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType} button!`);
+                runGame(gameType);
             }
         })
         
     }
+
+    //runGame('addition');
 })
 
-function runGame() {
+function runGame(gameType) {
 
     //Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2); 
+    } else {
+        alert(`Uknown game type: ${gameType}`);
+        throw `Uknown game type: ${gameType}. Aborting!`
+    }  
 }
 
 function checkAnswer() {
@@ -40,8 +49,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
